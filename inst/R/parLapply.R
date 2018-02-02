64c821791a17b178ad4a2944979ce39f9b81bccc
bonartm <bonartm@uni-koeln.de>
2018-02-02 11:39:12 +0100

    update parLapply.R

--- inst/R/parLapply.R
+++ inst/R/parLapply.R
@@ -6,8 +6,16 @@ loadPackagesOnCluster <- function(cl, packages){
 
 l <- readRDS("./tmp/lapply.rds")
 cl <- snow::makeMPIcluster(Rmpi::mpi.universe.size()-1)
+#cl <- snow::makeCluster(4)
 loadPackagesOnCluster(cl, l$packages)
+
+cat(format(Sys.time()), ": start calculation\n")
+
 res <- do.call(parallel::parLapplyLB, c(list(cl = cl, X = l$x, fun = l$fun), l$args))
+
 saveRDS(res, "./tmp/res.rds")
+
+cat(format(Sys.time()), ": finished calculation\n")
+
 snow::stopCluster(cl)
 Rmpi::mpi.quit()
