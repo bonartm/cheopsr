11c9c578357ad7a42f8e179fe987ab3ffb5009de
bonartm <bonartm@uni-koeln.de>
2018-03-19 15:49:48 +0100

    add cheops_run function

---
+++ inst/R/run.R
@@ -0,0 +1,29 @@
+library(Rmpi)
+library(snow)
+
+loadPackagesOnCluster <- function(cl, packages){
+  snow::clusterExport(cl, "packages", envir = environment())
+  res <- snow::clusterEvalQ(cl, invisible(lapply(packages, library, character.only = TRUE, logical.return = TRUE)))
+  all(unlist(res))
+}
+
+logger <- function(...){
+  cat("[", format(Sys.time()), "]", ..., "\n")
+}
+
+logger("initialize cluster")
+l <- readRDS("./tmp/run.rds")
+cl <- snow::makeMPIcluster(Rmpi::mpi.universe.size()-1)
+loadPackagesOnCluster(cl, l$packages)
+
+logger("start calculation")
+res <- do.call(l$fun, c(list(cl = cl), l$args))
+
+logger("save results")
+out <- paste0("./", l$jobname, "/res.rds")
+saveRDS(res, out)
+
+logger("stop cluster")
+stopCluster(cl)
+mpi.exit()
+logger("end script")
